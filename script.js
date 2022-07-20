1d06283cf903821af2ce9760bc5f302f4f475e1d
tanakajura <105241280+tanakajura@users.noreply.github.com>
2022-07-20 20:29:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,41 +13,66 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection){
+    let allocatePoint = 0; //if 1 its PC if 2 is player if 0 its a draw
+    
+    computerSelection = computerPlay().toLowerCase().trim();
+    playerSelection = prompt("Enter \"Rock\", \"Paper\" or \"Scissors\"", "").toLowerCase().trim();
 
-    computerSelection = computerPlay().toLowerCase();
-    playerSelection = prompt("Enter \"Rock\", \"Paper\" or \"Scissors\"", "").toLowerCase();
-
-    if(computerSelection == playerSelection){
-        return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " Its a Draw!");
+    if (!((playerSelection == "rock") || (playerSelection == "paper") || (playerSelection == "scissors"))){
+        return console.log("Wrong input! \n Insert Rock Paper or Scissors \n GOOD LUCK!")
     }else{
-        if((computerSelection == "rock") && (playerSelection == "Scissors")){
-            return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " Computer Wins!");
+        if(computerSelection == playerSelection){
+            allocatePoint = 0;
+            return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " Its a Draw!");
         }else{
-            if((computerSelection == "Scissors") && (playerSelection == "Rock")){
-                return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " You Win!");
+            if((computerSelection == "rock") && (playerSelection == "Scissors")){
+                allocatePoint = 1;
+                return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " Computer Wins!");
             }else{
-                if((computerSelection == "rock") && (playerSelection == "Paper")){
+                if((computerSelection == "Scissors") && (playerSelection == "Rock")){
+                    allocatePoint = 2;
                     return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " You Win!");
                 }else{
-                    if((computerSelection == "Paper") && (playerSelection == "Scissors")){
+                    if((computerSelection == "rock") && (playerSelection == "Paper")){
+                        allocatePoint = 2;
                         return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " You Win!");
                     }else{
-                        if((computerSelection == "Paper") && (playerSelection == "Rock")){
-                            return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " Computer Wins!");
+                        if((computerSelection == "Paper") && (playerSelection == "Scissors")){
+                            allocatePoint = 2;
+                            return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " You Win!");
                         }else{
-                            if((computerSelection == "Scissors") && (playerSelection == "Paper")){
+                            if((computerSelection == "Paper") && (playerSelection == "Rock")){
+                                allocatePoint = 1;
                                 return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " Computer Wins!");
+                            }else{
+                                if((computerSelection == "Scissors") && (playerSelection == "Paper")){
+                                    allocatePoint = 1;
+                                    return console.log("CPU Selected: " + computerSelection + "You Selected: " + playerSelection + " Computer Wins!");
+                            }
                         }
                     }
-                }
-            }      
+                }      
+            }
+        }
         }
     }
-    }
+    
 }
 
+let computerScore = 0;
+let playerScore = 0;
 
-
-playRound();
-
+for(let i = 0; i <=5; i++){
+    playRound();
+    if(allocatePoint == 1){
+        computerScore = computerScore + 1;
+    }else{
+        if(allocatePoint == 2){
+            playerScore = playerScore + 1;
+        }else{
+            i++;
+        }
+    }
+    i++;
+}
 
