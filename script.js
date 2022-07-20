9a6b78b122cd41a560ed0886c41f65c31991ab48
tanakajura <105241280+tanakajura@users.noreply.github.com>
2022-07-20 19:00:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function computerPlay( playerSelection, computerSelection){
+function computerPlay(){
+    let pcChoice = Math.floor(Math.random() * 31); 
+    
+    if (pcChoice <= 10){
+        return "Rock";
+    }else{
+        if (pcChoice >= 20){
+            return "Paper"; 
+        }else{
+            return "Scissors"
+        }
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    computerSelection = computerPlay();
     
 }
