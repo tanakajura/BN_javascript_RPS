b2d3d51f69eb811a723ae87ebd672d8dab5dc3ac
tanakajura <105241280+tanakajura@users.noreply.github.com>
2022-07-24 18:51:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,62 @@ function computerPlay(){
     let cpuChoice;
     switch(cpuChoice = Math.floor(Math.random() * 3) + 1){
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissors";
+            return "scissors";
     }
 }
 
-for(let i = 1; i <= 5; i++){
-    console.log(computerPlay());
+function playRound(playerSelection, computerSelection){  
+    playerSelection = prompt("Enter Rock, Paper or Scissors: ", "").toLowerCase().trim();
+    computerSelection = computerPlay();
+
+    if(computerSelection == "rock"){
+        switch(playerSelection){
+            case "paper":
+                return "You Win! Paper beats Rock";
+                break;
+            case "rock":
+                return "Draw!";
+                break;
+            case "scissors":
+                return "You Lose! Rock beats Scissors";
+                break;
+        }
+    }else{
+        if(computerSelection == "paper"){
+            switch(playerSelection){
+                case "paper":
+                    return "Draw!";
+                    break;
+                case "rock":
+                    return "You lose! Paper beats Rock";
+                    break;
+                case "scissors":
+                    return "You Win! Scissors beats Rock";
+                    break;
+            }
+        }else{
+            if(computerSelection == "scissors"){
+                switch(playerSelection){
+                    case "paper":
+                        return "You Lose! Scissors beats paper";
+                        break;
+                    case "rock":
+                        return "You win! Rock beats Scissors";
+                        break;
+                    case "scissors":
+                        return "Draw!";
+                        break;
+                }
+            }
+        }
+    }
 }
-/*function playRound(playerSelection, computerSelection){
 
-} */
-
+console.log(playRound());
 
 
 
